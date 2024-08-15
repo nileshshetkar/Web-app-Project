ae67e6637a70e126dac9ccb3d4273e3d5fbc8504
Nilesh Shetkaar <122077009+nileshshetkar@users.noreply.github.com>
2024-08-15 17:56:10 +0530

    Add files via upload
    firdt upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents the default form submission
+    
+    alert('Thank you for your message! We will get back to you soon.');
+    
+    // You can add form submission logic here (e.g., sending data to a server)
+});
